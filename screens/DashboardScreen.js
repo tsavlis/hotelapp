a6f4fb923a7131a473f6aeb38568e570f1e8cb6e
tsavlis <tsavlis.thanos@gmail.com>
2019-11-10 15:36:27 +0200

    new img

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -3,7 +3,7 @@ import { View, Text, ImageBackground } from "react-native";
 
 import * as actions from "../src/store/actions";
 import { connect } from "react-redux";
-import home from "../assets/home1.jpeg";
+import home from "../assets/hom2.jpg";
 import FadeInView from "./FadeInView";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
@@ -49,20 +49,20 @@ class DashboardScreen extends Component {
           >
             <View style={styles.row}>
               <View style={styles.card}>
-                <Ionicons name="md-clock" size={35} color="black" />
+                <Ionicons name="md-clock" size={35} color="white" />
                 <Text style={styles.item}>
                   {this.props.services[0] ? this.props.services[0].name : null}
                 </Text>
               </View>
               <View style={styles.card}>
-                <FontAwesome name="home" size={30} color="black" />
+                <FontAwesome name="home" size={30} color="white" />
 
                 <Text style={styles.item}>
                   {this.props.services[1] ? this.props.services[1].name : null}
                 </Text>
               </View>
               <View style={styles.card}>
-                <FontAwesome name="bell" size={30} color="black" />
+                <FontAwesome name="bell" size={30} color="white" />
 
                 <Text style={styles.item}>
                   {this.props.services[2] ? this.props.services[2].name : null}
@@ -71,20 +71,20 @@ class DashboardScreen extends Component {
             </View>
             <View style={styles.row}>
               <View style={styles.card}>
-                <FontAwesome name="taxi" size={30} color="black" />
+                <FontAwesome name="taxi" size={30} color="white" />
 
                 <Text style={styles.item}>
                   {this.props.services[3] ? this.props.services[3].name : null}
                 </Text>
               </View>
               <View style={styles.card}>
-                <Ionicons name="md-chatboxes" size={30} color="black" />
+                <Ionicons name="md-chatboxes" size={30} color="white" />
                 <Text style={styles.item}>
                   {this.props.services[4] ? this.props.services[4].name : null}
                 </Text>
               </View>
               <View style={styles.card}>
-                <FontAwesome name="map" size={30} color="black" />
+                <FontAwesome name="map" size={30} color="white" />
 
                 <Text style={styles.item}>
                   {this.props.services[5] ? this.props.services[5].name : null}
@@ -119,11 +119,11 @@ const styles = {
   },
   item: {
     fontFamily: "regu1",
-    color: "black",
+    color: "white",
     marginTop: 5
   },
   card: {
-    backgroundColor: "#bffffd",
+    backgroundColor: "gray",
     height: 100,
     width: "33%",
     margin: 10,
@@ -146,7 +146,7 @@ const styles = {
   text: {
     fontSize: 30,
     fontFamily: "regu1",
-    color: "#00fff7"
+    color: "white"
     // marginBottom: 150
   },
   subttext: {
